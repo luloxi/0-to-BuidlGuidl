d6e3ad41e9a88edd35684d349558b6d5b50ad85a
Lulox <ligma@duck.com>
2023-08-29 00:29:48 -0300

    Attempt to solve deploy error again

--- packages/nextjs/pages/easy-share.tsx
+++ packages/nextjs/pages/easy-share.tsx
@@ -10,7 +10,13 @@ const Home: NextPage = () => {
       <MetaHeader />
       <div className="hero  bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src="/experience.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+          <Image
+            alt="Have experience? No, sir. I hope you will gain some here. You're hired."
+            src="/experience.jpg"
+            width={500}
+            height={500}
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
           <div>
             <h1 className="text-5xl font-bold">
               <span className="block text-4xl font-bold mb-2">
