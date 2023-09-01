e6404cdbaa0d97644a89816a7359ed61c930bd84
Lulox <ligma@duck.com>
2023-08-31 23:39:42 -0300

    Added copylink to share page

--- packages/nextjs/pages/share.tsx
+++ packages/nextjs/pages/share.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
 import Image from "next/image";
+import copy from "clipboard-copy";
+// You can import a copy icon from Heroicons or any other icon library you prefer
 // import Link from "next/link";
 import type { NextPage } from "next";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import {
+  Bars3Icon,
+  BoltIcon,
+  BookOpenIcon, // BugAntIcon,
+  HomeIcon,
+  ShareIcon, // SparklesIcon,
+} from "@heroicons/react/24/outline";
+// import { Copy } from "@heroicons/react/24/solid";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
+  const [copied, setCopied] = useState(false);
+  const url = "https://0-to-buidlguidl.vercel.app";
+
+  const handleCopy = () => {
+    copy(url);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+  };
+
   return (
     <>
       <MetaHeader />
@@ -16,7 +39,27 @@ const Home: NextPage = () => {
           height={500}
           className="max-w-xs rounded-lg shadow-2xl "
         />
-        <p>Add a way to easily copy the URL to this page and share to social buttons?</p>
+        <div className="flex items-center my-4 space-x-2">
+          <input
+            type="text"
+            value={url}
+            readOnly
+            placeholder="https://0-to-buidlguidl.vercel.app"
+            className="p-2 border rounded-lg w-60 text-primary"
+            // onClick={(e) => e.target.select()}
+          />
+          <CopyToClipboard text={url}>
+            <button
+              onClick={handleCopy}
+              className={`p-2 rounded-lg ${
+                copied ? "bg-green-500" : "bg-blue-500"
+              } text-white flex items-center focus:outline-none`}
+            >
+              {/* <CopyIcon className="w-4 h-4 mr-2" /> */}
+              {copied ? "Copied!" : "Copy link"}
+            </button>
+          </CopyToClipboard>
+        </div>
       </div>
     </>
   );
